120fbab72565d91f2a9ea8804b07f372f0fdfeda
shengxiang <codeskyblue@gmail.com>
2017-08-30 15:01:32 +0800

    add screen client

--- app/src/androidTest/java/com/github/uiautomator/stub/Log.java
+++ app/src/androidTest/java/com/github/uiautomator/stub/Log.java
@@ -24,17 +24,17 @@
 package com.github.uiautomator.stub;
 
 public class Log {
-	public static final String TAG = "UIAutomatorStub";
-	
-	public static void d(String msg) {
-		android.util.Log.d(TAG, msg);
-	}
+    public static final String TAG = "UIAutomatorStub";
 
-	public static void i(String msg) {
-		android.util.Log.i(TAG, msg);
-	}
+    public static void d(String msg) {
+        android.util.Log.d(TAG, msg);
+    }
 
-	public static void e(String msg) {
-		android.util.Log.e(TAG, msg);
-	}
+    public static void i(String msg, String s) {
+        android.util.Log.i(TAG, msg);
+    }
+
+    public static void e(String msg) {
+        android.util.Log.e(TAG, msg);
+    }
 }
---
+++ app/src/main/java/com/github/uiautomator/ScreenClient.java
@@ -0,0 +1,47 @@
+package com.github.uiautomator;
+
+import android.graphics.Bitmap;
+
+import java.io.ByteArrayOutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by hzsunshx on 2017/8/30.
+ */
+
+public class ScreenClient {
+    private static final String PROCESS_NAME = "screen.cli";
+    private static final String VERSION = "1.0";
+
+    public static void main(String[] args) {
+        setArgV0(PROCESS_NAME);
+
+        // Requires SDK >= 18
+        Method injector = null;
+        try {
+            injector = Class.forName("android.view.SurfaceControl").getDeclaredMethod("screenshot", new Class[]{Integer.TYPE, Integer.TYPE});
+            Bitmap bmp = (Bitmap) injector.invoke(null, new Object[]{Integer.valueOf(0), Integer.valueOf(0)});
+
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.JPEG, 75, bout);
+            System.out.println("screenshot success " + bmp.getHeight() + ", " + bmp.getWidth());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
+    }
+
+    private static void setArgV0(String text) {
+        try {
+            Method setter = android.os.Process.class.getMethod("setArgV0", String.class);
+            setter.invoke(android.os.Process.class, text);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
