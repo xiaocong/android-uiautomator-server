743922b7cd49603e622f73642cb2c440e43c18ca
diaojunxian <diaojunxian@huami.com>
2018-05-14 21:53:40 +0800

    修改变量为直接声明赋值

--- app/src/main/java/com/github/uiautomator/AdbBrodcastReceiver.java
+++ app/src/main/java/com/github/uiautomator/AdbBrodcastReceiver.java
@@ -27,13 +27,10 @@ public class AdbBrodcastReceiver extends BroadcastReceiver {
             mockGPS = new MockLocationProvider(LocationManager.GPS_PROVIDER, context);
             mockWifi = new MockLocationProvider(LocationManager.NETWORK_PROVIDER, context);
 
-            double lat, lon, alt;
-            float accurate;
-
-            lat = Double.parseDouble(intent.getStringExtra("lat") != null ? intent.getStringExtra("lat") : "0");
-            lon = Double.parseDouble(intent.getStringExtra("lon") != null ? intent.getStringExtra("lon") : "0");
-            alt = Double.parseDouble(intent.getStringExtra("alt") != null ? intent.getStringExtra("alt") : "0");
-            accurate = Float.parseFloat(intent.getStringExtra("accurate") != null ? intent.getStringExtra("accurate") : "0");
+            double lat = Double.parseDouble(intent.getStringExtra("lat") != null ? intent.getStringExtra("lat") : "0");
+            double lon = Double.parseDouble(intent.getStringExtra("lon") != null ? intent.getStringExtra("lon") : "0");
+            double alt = Double.parseDouble(intent.getStringExtra("alt") != null ? intent.getStringExtra("alt") : "0");
+            float accurate = Float.parseFloat(intent.getStringExtra("accurate") != null ? intent.getStringExtra("accurate") : "0");
             Log.i(TAG, String.format("setting mock to Latitude=%f, Longitude=%f Altitude=%f Accuracy=%f", lat, lon, alt, accurate));
             mockGPS.pushLocation(lat, lon, alt, accurate);
             mockWifi.pushLocation(lat, lon, alt, accurate);
